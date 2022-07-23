b39e267f779eb777d8f51971f2dcd7e84d0c8c6a
Dani Meleh <danim5410@gmail.com>
2022-07-23 20:27:13 +0300

    First commit

---
+++ models/student_model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const take_course_schema = new mongoose.Schema({
+  cid: {
+    String,required:true,
+    validate: { validator:(v)=>(v.length == 5 && v.trim() != '')}
+  },
+  grade:{
+    Number,
+    required:true,
+    min:0,
+    max:100
+  }
+});
+
+const student_schema = new mongoose.Schema({
+  id:{String,required:true,
+    validate: { validator:(v)=>(v.length == 9 && v.trim() != '')}
+  },
+  name:{String,required:true,
+    validate: { validator:(v)=>(v.length == 1 && v.trim() != '')}
+  },
+  city:{String,
+    validate: { validator:(v)=>(v.length == 1 && v.trim() != '')}
+  },
+  toar:{
+    String,required:true,
+    enum: ['ba','ma','phd']
+  },
+  courses: [take_course_schema],
+}, {collection: 'Academy'});
+
+const user = mongoose.model('',student_schema)
+
+module.exports = user;
