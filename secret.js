8a95b8cd9aa0a956a84092857fb6cd0bbf3ba46b
Yuval <yuval1abc@gmail.com>
2022-07-26 17:53:33 +0300

    test with dani

--- secret.js
+++ secret.js
@@ -1,3 +1,7 @@
 console.log("Dani tizki the king of the universe");
 console.log("Dani tizki the king of the universe");
 console.log("Dani tizki the king of the universe");
+let x=1;
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
