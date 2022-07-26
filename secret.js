a392d4423eeea67f27baccd3a885496a14b70a66
Yuval <yuval1abc@gmail.com>
2022-07-26 18:20:42 +0300

    test of branch

--- secret.js
+++
@@ -1,8 +0,0 @@
-console.log("Dani tizki the king of the universe");
-console.log("Dani tizki the king of the universe");
-console.log("Dani tizki the king of the universe");
-let x=1;
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
-"dfgdgsgdgsd";
